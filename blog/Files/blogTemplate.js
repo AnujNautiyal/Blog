c0c3e5c48dc7055c6fc49329831588d51e00a190
AnujNautiyal <anuj.nautiyal@paytm.com>
2016-04-14 13:48:47 +0530

    First Commit

---
+++ blog/Files/blogTemplate.js
@@ -0,0 +1,10 @@
+/*display post page*/
+var post={
+main : function(req,res)
+{
+  console.log("blogTemplate");
+  res.render('blogTemplate');
+}
+
+}
+module.exports = post;
---
+++ blog/Files/cipher.js
@@ -0,0 +1,15 @@
+var crypto = require('crypto');
+
+var crypt={
+
+  cipher: function(text){
+    var algorithm = 'aes-256-ctr';
+    var password = 'd6F3Efeq';
+    var cipher = crypto.createCipher(algorithm,password);
+    var crypted = cipher.update(text,'utf8','hex')
+    crypted += cipher.final('hex');
+    return crypted;
+  }
+}
+
+module.exports=crypt;
